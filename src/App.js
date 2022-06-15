4a11563bcb28d933e66c66aeaa597f1d83c633b1
Avishekh Jaiswal <j.avishekh2021@gmail.com>
2022-06-15 20:32:31 +0530

    'removed imports from app.js"

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Contact from './Components/Contact/Contact'
 import Experience from './Components/Experience/Experience';
 import Footer from './Components/Footer/Footer';
 import Header from './Components/Header/Header';
-import Testimonials from './Components/Testimonials/Testimonials';
+
 import Nav from './Components/Nav/Nav';
-import Services from './Components/Services/Services'
+
 import './index.css';
 import Portfolio from './Components/Portfolio/Portfolio';
 import Loader from './Components/Loader.jsx/Loader';
